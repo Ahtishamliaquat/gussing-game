9af21cdfb3d6e0b025d8160680d2895edc77ecc1
Ahtishamliaquat <162324518+Ahtishamliaquat@users.noreply.github.com>
2024-04-16 11:00:19 +0500

    first comment

---
+++ index.ts
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import Choices from "inquirer/lib/objects/choices.js";
+const randomNumber =Math.floor(Math.random()*10 +1)
+const answer =await inquirer.prompt([
+    {name:"userGussingGame",
+    type:"number",
+    message: "gussed a number between 1-10",
+    }
+])
+if(answer.userGussingGame === randomNumber){
+    console.log ("congtratulation! you gussed true number")
+}
+else{console.log("you gussed wrong number")}
